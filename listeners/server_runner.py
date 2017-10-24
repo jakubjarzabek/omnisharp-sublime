19cf682e783328f0b87e37c24db329066173532e
Jakub Jarzabek <jjarzabek@gmail.com>
2017-10-24 22:20:16 +0200

    Fixing multiple omnisharp server starting

--- listeners/server_runner.py
+++ listeners/server_runner.py
@@ -5,9 +5,17 @@ from ..lib import omnisharp
 
 
 class OmniSharpServerRunnerEventListener(sublime_plugin.EventListener):
+
+    def __init__(self):
+        self.initialized = False
+
     def on_activated(self, view):
         if not helpers.is_csharp(view):
             return
-            
+        if self.initialized:
+            print('Server already initialized')
+            return
+        print('Starting server: ' + view.name() + ' ' + view.file_name() + ' ' + str(self.initialized))
+        self.initialized = True
         omnisharp.create_omnisharp_server_subprocess(view)
 
