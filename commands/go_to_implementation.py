1673d1dffac0763e95a2fdf3eecf5afa61ce073a
Jakub Jarzabek <jjarzabek@gmail.com>
2017-11-16 20:19:21 +0100

    Consistent behavior of find usages and go to implementation command.
    Change go to implementation to match find usages command.

--- commands/go_to_implementation.py
+++ commands/go_to_implementation.py
@@ -9,47 +9,37 @@ class OmniSharpGoToImplementation(sublime_plugin.TextCommand):
     data = None
 
     def run(self, edit):
-        if self.data is None:
-            omnisharp.get_response(
-                self.view, '/findimplementations', self._handle_findimplementations)
-        else:
-            self._show_usage_view(edit)
+        omnisharp.get_response(
+                self.view, '/findimplementations', self._show_usage_view)
 
-    def _handle_findimplementations(self, data):
-        print(data)
-        if data is None:
+    def _show_usage_view(self, data):
+        if data is None or data["QuickFixes"] is None:
             return
-        self.data = data
-        self.view.run_command('omni_sharp_go_to_implementation')
 
-    def _show_usage_view(self, edit):
         print('gotoimplementation is :')
-        print(self.data)
-        self.quickitems = [];
-        if "QuickFixes" in self.data and self.data["QuickFixes"] != None:
-            for i in self.data["QuickFixes"]:
-                print(i)
-                self.quickitems.append(i["Text"].strip())
-        if len(self.quickitems) > 0:
-            if len(self.quickitems) == 1:
-               self.on_done(0) 
-            else:
-                self.view.window().show_quick_panel(self.quickitems, self.on_done)
+        print(data)
+
+        usages = data["QuickFixes"]
+
+        if len(usages) == 0:
+            return
+
+        window = self.view.window()
+
+        def on_done(i):
+            if i is not -1:
+                window.open_file('{}:{}:{}'.format(usages[i]["FileName"], usages[i]["Line"] or 0, usages[i]["Column"] or 0), sublime.ENCODED_POSITION)
+
+        def on_highlight(i):
+            if i is not -1:
+                window.open_file('{}:{}:{}'.format(usages[i]["FileName"], usages[i]["Line"] or 0, usages[i]["Column"] or 0), sublime.ENCODED_POSITION | sublime.TRANSIENT)
+
+        if len(usages) == 1:
+            on_done(0)
+            return
+
+        items = [[u["Text"].strip(), u["FileName"] + " Line : " + str(u["Line"])] for u in usages]
+        window.show_quick_panel(items, on_done, on_highlight=on_highlight)
 
     def is_enabled(self):
         return helpers.is_csharp(self.view)
-
-    def on_done(self, index):
-        values = self.data["QuickFixes"]
-        item = values[index]
-        v = self.view.window().open_file(item["FileName"])
-        sublime.set_timeout(lambda: self.file_opened(v, item), 10)
-
-    def file_opened(self, view, item):
-    	if not view.is_loading():
-    		print('loaded')
-    		view.run_command("goto_line", {"line": item["Line"]})
-    	else:
-    		print('not loaded, trying again')
-    		sublime.set_timeout(lambda: self.file_opened(view, item), 10)
-        
